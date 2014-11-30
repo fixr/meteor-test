c4d167508ee15c38f6dd52a496045d713ac5abbc
Freddy Peña <freddy.pena@the-cocktail.com>
2014-11-30 19:40:04 +0100

    Collections

--- codetails.js
+++ codetails.js
@@ -1,16 +1,24 @@
 // 1. HTML as templates.
 // 2. Spacebars {{ ... }}. {{# each }}, {{# if }}, etc...
 // 3. Javascript "helpers" for templates
+// 4. Collections
+//    meteor mongo
+//    db.talks.insert({ title: "Conquistando al mundo, un buffer de Emacs a la vez.", createdAt: new Date() });
 
+Talks = new Mongo.Collection("talks");
 
 if (Meteor.isClient) {
   // This code only runs on the client
   Template.body.helpers({
-    talks: [
-      { text: "Arreglando el Asset Pipeline" },
-      { text: "Mi hijo, llamado Godzilla" },
-      { text: "¿Cómo me cargué los assets del bucket? #bucketgate" }
-    ]
+    // talks: [
+    //   { title: "Arreglando el Asset Pipeline" },
+    //   { title: "Mi hijo, llamado Godzilla" },
+    //   { title: "¿Cómo me cargué los assets del bucket? #bucketgate" }
+    // ]
+
+    talks: function () {
+      return Talks.find({});
+    }
   });
 }
 
