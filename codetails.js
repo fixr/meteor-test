8d3de2e46f2f2692701c390dfd5f94a9f40e6bd4
Freddy Peña <freddy.pena@the-cocktail.com>
2014-11-30 20:05:01 +0100

    Inserting + events

--- codetails.js
+++ codetails.js
@@ -4,6 +4,9 @@
 // 4. Collections
 //    meteor mongo
 //    db.talks.insert({ title: "Conquistando al mundo, un buffer de Emacs a la vez.", createdAt: new Date() });
+// 5. Events
+//    real-time ability
+// 6. Inserting values to DB and sorting on the view
 
 Talks = new Mongo.Collection("talks");
 
@@ -17,9 +20,30 @@ if (Meteor.isClient) {
     // ]
 
     talks: function () {
-      return Talks.find({});
+      return Talks.find({}, { sort: { createdAt: -1 }});
     }
   });
+
+  Template.body.events({
+    "submit .new-talk": function (event) {
+      // This function is called when the new talk form is submitted
+      console.log(event);
+
+      var title = event.target.title.value;
+
+      Talks.insert({
+        title: title,
+        createdAt: new Date() // current time
+      });
+
+      // Clear form
+      event.target.title.value = "";
+
+      // Prevent default form submit
+      return false;
+    }
+  });
+
 }
 
 if (Meteor.isServer) {
